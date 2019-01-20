5c5532dd7e05796b16754259ff591d4466e665ed
Hadi Hoseini <mir.hadi.peyrov@gmail.com>
2019-01-20 10:08:47 +0330

    Initial Solution

---
+++ RedHatOpenShiftTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RedHatOpenShiftTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
